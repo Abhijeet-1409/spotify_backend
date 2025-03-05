3368e72dbfa81fcbbdb42a5fb57e46648686012f
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-03-05 19:18:55 +0530

    ADD ALBUMDB MODEL

---
+++ app/models/album.py
@@ -0,0 +1,20 @@
+from bson import ObjectId
+from datetime import datetime, timezone 
+from pydantic import BaseModel,HttpUrl,Field
+
+class AlbumDB(BaseModel) :
+    title: str = Field(title="Title",description="Album's title")
+    artist: str = Field(title="Artist",description="Album's artist")
+    image_url: HttpUrl = Field(title="Image Url",description="Album's cover image")
+    release_year: int = Field(title="Release Year",description="Album's release year")
+    songs: list[ObjectId] = Field(title="Songs",description="Album's Song")
+    created_at: datetime = Field(
+                                    default_factory=lambda: datetime.now(timezone.utc), 
+                                    title="Created At",
+                                    description="Timestamp when the album was created"
+                                )
+
+    class Config :
+        arbitrary_types_allowed = True
+
+
