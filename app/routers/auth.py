ae6f18a641413c3f6357b6dad5cc11e8cbffae8e
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-03-27 21:57:50 +0530

    ADD AUTH API

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,26 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Depends, Body, status
+
+from app.schemas.user import UserIn
+from app.services.auth import AuthService
+from app.dependencies.dependencies import get_auth_service
 
 router = APIRouter(
     prefix="/api/auth",
     tags=["auth"]
 )
+
+@router.post('/callback')
+async def handle_auth_callback(
+        user_data: Annotated[UserIn, Body()],
+        auth_service: Annotated[AuthService, Depends(get_auth_service)]
+    ) -> JSONResponse:
+
+    content: dict = await auth_service.authCallback(user_data=user_data)
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=content,
+    )
