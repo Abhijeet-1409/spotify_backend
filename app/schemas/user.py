1ef42458dfa1f379a865077bfb468de2489acb1e
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-04-02 00:44:38 +0530

    UPDATE USER MODEL AND SCHEMA

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, Field, EmailStr, HttpUrl
 
 class UserIn(BaseModel):
     _id: str = Field(title="_id",description="clerkId value is used as user's _id")
-    username: str = Field(title="Username",description="User's username must be unique")
+    first_name: str = Field(title="First Name",description="User's first name")
+    last_name: str = Field(title="Last Name",description="User's last name")
     email: EmailStr = Field(title="Email",description="User's email must be unique")
     image_url: HttpUrl = Field(title="Image Url",description="User's profile image")
