086965ba47ec254b0f333d780198e70cb116b667
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-04-09 18:46:40 +0530

    ADD GET STATS ROUTE

--- app/routers/stat.py
+++ app/routers/stat.py
@@ -1,6 +1,25 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+
+from app.schemas.stat import Stats
+from app.services.stat import StatService
+from app.dependencies.dependencies import (
+    require_admin,
+    get_stat_service
+)
+
 
 router = APIRouter(
-    prefix="/api/stat",
-    tags=["stat"]
+    prefix="/api/stats",
+    tags=["stats"],
+    dependencies=[Depends(require_admin)]
 )
+
+
+@router.get("/")
+async def get_stats(stat_service: Annotated[StatService, Depends(get_stat_service)]) -> Stats:
+
+    stats: Stats = await stat_service.fetch_stats()
+
+    return stats
