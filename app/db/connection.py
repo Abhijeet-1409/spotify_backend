5d7a7d69cdc7030d492546cf9ba3ac4c7ba4f8a1
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-04-15 16:22:56 +0530

    REFACTOR DATABASECONNECTION CLASS

--- app/db/connection.py
+++ app/db/connection.py
@@ -3,20 +3,23 @@ from motor.motor_asyncio import AsyncIOMotorClient,AsyncIOMotorDatabase,AsyncIOM
 
 class DatabaseConnection() :
     _instance = None
+    _initialized = False
 
-    def __new__(cls, settings: Settings) :
-        if cls._instance is None :
+    def __new__(cls, *args, **kwargs) :
+        if not cls._instance :
             cls._instance = super(DatabaseConnection,cls).__new__(cls)
-            cls._instance._init_db(settings)
         return cls._instance
 
-    def _init_db(self, settings: Settings):
+    def __init__(self, settings: Settings):
+        if DatabaseConnection._initialized :
+            return
         self.client: AsyncIOMotorClient = AsyncIOMotorClient(settings.MONGO_URI)
         self.db: AsyncIOMotorDatabase = self.client.get_database(settings.MONGO_DBNAME)
         self.users: AsyncIOMotorCollection = self.db.get_collection("users")
         self.albums: AsyncIOMotorCollection = self.db.get_collection("albums")
         self.songs: AsyncIOMotorCollection = self.db.get_collection("songs")
         self.messages: AsyncIOMotorCollection = self.db.get_collection("messages")
+        DatabaseConnection._initialized = True
 
     async def create_index(self) -> None:
         await self.users.create_index([("email", 1)], unique=True)
