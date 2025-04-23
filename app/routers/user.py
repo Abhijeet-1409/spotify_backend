f1534b55cc5fc6ad82392ce684be9dcf2dc862bc
Abhijeet-1409 <abhijeetjoy14@gmail.com>
2025-04-23 16:13:41 +0530

    ADD GET ALL USERS ROUTE

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,6 +1,31 @@
-from fastapi import APIRouter
+from typing import Annotated, List
+
+from fastapi import APIRouter, Depends
+
+from clerk_backend_api.models.user import User as ClerkUser
+
+from app.schemas.user import UserOut
+from app.services.user import UserService
+from app.dependencies.dependencies import (
+    protect_route,
+    get_user_service
+)
+
 
 router = APIRouter(
-    prefix="/api/user",
-    tags=["user"]
+    prefix="/api/users",
+    tags=["users"]
 )
+
+
+@router.get("/")
+async def get_all_users(
+        user: Annotated[ClerkUser, Depends(protect_route)],
+        user_service: Annotated[UserService, Depends(get_user_service)]
+    ) -> List[UserOut]:
+
+    current_user_id: str = user.id
+    user_out_list: List[UserOut] = await user_service.fetch_all_users(current_user_id)
+
+    return user_out_list
+
